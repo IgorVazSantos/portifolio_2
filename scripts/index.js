75ce8ad98a3d1da2c51181eb5f0f3051a73fd580
IgorVazSantos <g.igor@live.com>
2023-12-13 22:38:46 -0300

    inclusão do logo

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,19 @@
-let anocopyright = document.getElementById("copyright");
-let anoAtual = new Date().getFullYear();
+const anocopyright = document.getElementById("copyright");
+const anoAtual = new Date().getFullYear();
 
 anocopyright.innerHTML = '&copy '+`${anoAtual}` +' - Igor Vaz';
+
+
+
+const logo = document.getElementById("logo");
+const novaImagem = document.createElement("img");
+  novaImagem.src = "./imagens/Meulogo_200px.png";
+  novaImagem.alt = "Logo";
+  novaImagem.id = "logo200px";
+
+function changeToLogo() {
+    logo.innerHTML = "";
+    logo.appendChild(novaImagem)
+}
+
+setTimeout(changeToLogo, 4000)
